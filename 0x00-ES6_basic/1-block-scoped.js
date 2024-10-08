dd4d8bda9e9fbfa9318e3f6ae8cae5c0311ec7d4
ermi4sol <ermi4sol@gmail.com>
2024-10-08 12:25:23 +0300

    fix some bugs on 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,16 +1,11 @@
 export default function taskBlock(trueOrFalse) {
     const task = false;  // Original value of task
     const task2 = true;  // Original value of task2
-  
-    // Use let to define new variables that are scoped to the if block
-    let newTask = task; // Start with original value
-    let newTask2 = task2; // Start with original value
-  
+
     if (trueOrFalse) {
-      newTask = true;    // Change newTask based on condition
-      newTask2 = false;  // Change newTask2 based on condition
+      task = true;    // Change newTask based on condition
+      task2 = false;  // Change newTask2 based on condition
     }
-  
+
     return [newTask, newTask2]; // Return the modified or original values
-  }
-  
+}
