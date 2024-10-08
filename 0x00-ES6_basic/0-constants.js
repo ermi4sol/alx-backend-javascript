7139dd02f81a87d99a93f2f00f2625c9f0583a6e
ermi4sol <ermi4sol@gmail.com>
2024-10-08 12:06:45 +0300

    fix task0

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,6 +1,6 @@
-// Use const in taskFirst
-/** export function taskFirst() {
-    const task = 'I prefer const when I can.';
+// Use const's in taskFirst
+export function taskFirst() {
+    const task = "I prefer const's when I can.";
     return task;
 }
 
@@ -16,21 +16,3 @@ export function taskNext() {
 
     return combination;
 }
-**/
-
-// Use const only once in taskFirst
-export function taskFirst() {
-    return 'I prefer const when I can.';
-  }
-  
-  // getLast function remains the same
-  export function getLast() {
-    return ' is okay';
-  }
-  
-  // Use let only once in taskNext
-  export function taskNext() {
-    let combination = 'But sometimes let' + getLast(); // Combine the assignment and the addition
-    return combination;
-  }
-  
